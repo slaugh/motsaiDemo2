c6823eb13530b6e77e48b7d54eec34c8c8568acb
Scott Laughlin <scott.laughlin15@gmail.com>
2016-09-30 16:21:39 +0200

    A few small tweaks to the Bluetooth checking code and some updates the basic AWS integration plan

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -23,13 +23,14 @@ public class AndroidLauncher extends AndroidApplication {
 
         //Problem: Send a constant stream of quaternions up to the cloud for machine learning processing
 
-        //Solution: Collect quaternion data into a buffer
-        //          Fill a buffer with the maximum amount of data that can be transmitted in an AWS packet
-        //          When the buffer is greater than or equal to the packet capacity then flush the buffer to AWS
+        //Solution: 1. Temporary -> Generate data every couple milisecconds until we have accesss to the hardware
+        //          2. Collect quaternion data into a buffer
+        //          3. Fill a buffer with the maximum amount of data that can be transmitted in an AWS packet
+        //          4. When the buffer is greater than or equal to the packet capacity then flush the buffer to AWS
 
-        //Questions: What type of JAVA buffer to use
-        //           How do we queue and dequeue
-        //           What else can we
+        //Questions: 1. What type of JAVA buffer to use
+        //           2. How do we queue and dequeue
+        //           3. What is the maximum size of the packets going to AWS... let's test this
 
 
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
@@ -53,11 +54,19 @@ public class AndroidLauncher extends AndroidApplication {
             //Case 2: Bluetooth exists but is not enabled
             if (!mBluetoothAdapter.isEnabled()) {
                 // Bluetooth is not enable :)
+                //Explain to the user that he needs to enable his bluetooth
+                Context context = getApplicationContext();
+                CharSequence text = "Bluetooth is not enabled, please activate your Bluetooth";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                //Send the user to go enable bluetooth and come back when finished
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             } else  {
-                //Case 3: Bluetooth is enabled
-                Intent intent = new Intent(this, BLEDeviceScanActivity.class); //THIS IS WHERE THE ACTION IS!
+                //Case 3: Bluetooth is enabled so start the program
+                Intent intent = new Intent(this, BLEDeviceScanActivity.class);
                 startActivity(intent);
             }
         }
