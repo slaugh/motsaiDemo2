9dd76b5070f296dcfff7ceb0ae74e55799b55ac7
Scott Laughlin <scott.laughlin15@gmail.com>
2016-09-30 16:09:48 +0200

    Finished graceful degradation code, added a little plan for testing AWS code

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -19,12 +19,27 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+        //TODO: Play around with AWS drivers here
+
+        //Problem: Send a constant stream of quaternions up to the cloud for machine learning processing
+
+        //Solution: Collect quaternion data into a buffer
+        //          Fill a buffer with the maximum amount of data that can be transmitted in an AWS packet
+        //          When the buffer is greater than or equal to the packet capacity then flush the buffer to AWS
+
+        //Questions: What type of JAVA buffer to use
+        //           How do we queue and dequeue
+        //           What else can we
+
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		Invaders invaders = new Invaders(invaderInterface); //Why can't I call an invaderInterface here...
 		initialize(invaders, config);
 
         //Check to see if Bluetooth Adapters are enabled and available
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //Case 1: No Bluetooth on this device
         if (mBluetoothAdapter == null) {
             Context context = getApplicationContext();
             CharSequence text = "Bluetooth is not available, use a device that has bluetooth";
@@ -33,19 +48,18 @@ public class AndroidLauncher extends AndroidApplication {
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
             // Device does not support Bluetooth
+            this.finishAffinity();
         } else {
+            //Case 2: Bluetooth exists but is not enabled
             if (!mBluetoothAdapter.isEnabled()) {
                 // Bluetooth is not enable :)
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             } else  {
-                //Bluetooth is enabled
-                Intent intent = new Intent(this, BLEDeviceScanActivity.class);
+                //Case 3: Bluetooth is enabled
+                Intent intent = new Intent(this, BLEDeviceScanActivity.class); //THIS IS WHERE THE ACTION IS!
                 startActivity(intent);
             }
         }
-
-
-
 	}
 }
